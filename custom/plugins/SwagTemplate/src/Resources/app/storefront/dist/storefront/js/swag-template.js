bd8b8d64e35f72febe729b41df24fc7610440a3b
Oliver Schafeld <oliver@schafeld.com>
2025-11-11 13:01:13 +0100

    add new custom theme that replaces main logo

---
+++ custom/plugins/SwagTemplate/src/Resources/app/storefront/dist/storefront/js/swag-template.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["swag-template"],{
+
+/***/ "c6X6":
+/*!*********************************************************************************!*\
+  !*** /app/custom/plugins/SwagTemplate/src/Resources/app/storefront/src/main.js ***!
+  \*********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYzZYNi5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///c6X6\n");
+
+/***/ })
+
+},[["c6X6","runtime"]]]);
---
+++ custom/plugins/SwagTemplate/src/SwagTemplate.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SwagTemplate;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class SwagTemplate extends Plugin implements ThemeInterface
+{
+}
