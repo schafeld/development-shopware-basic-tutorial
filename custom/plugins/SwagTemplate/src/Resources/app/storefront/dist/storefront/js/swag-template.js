9da1c7134ac7c98f1db5421291d631ebafe86941
Oliver Schafeld <oliver@schafeld.com>
2025-11-12 14:48:45 +0100

    add custom javascript to plugin

--- custom/plugins/SwagTemplate/src/Resources/app/storefront/dist/storefront/js/swag-template.js
+++ custom/plugins/SwagTemplate/src/Resources/app/storefront/dist/storefront/js/swag-template.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYzZYNi5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///c6X6\n");
+eval("console.info('SwagTemplate main.js loaded');//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYzZYNi5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8vYXBwL2N1c3RvbS9wbHVnaW5zL1N3YWdUZW1wbGF0ZS9zcmMvUmVzb3VyY2VzL2FwcC9zdG9yZWZyb250L3NyYy9tYWluLmpzP2I2ODAiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5pbmZvKCdTd2FnVGVtcGxhdGUgbWFpbi5qcyBsb2FkZWQnKTsiXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///c6X6\n");
 
 /***/ })
 
