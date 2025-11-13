c6451861ff99f8761ac19b9e5fc4b1f5cbfd92e9
Oliver Schafeld <oliver@schafeld.com>
2025-11-13 12:56:38 +0100

    animate the cart-icon in header when ATC button is clicked

--- custom/plugins/SwagTemplateAdvanced/src/Resources/app/storefront/src/script/rumble-button.js
+++ custom/plugins/SwagTemplateAdvanced/src/Resources/app/storefront/src/script/rumble-button.js
@@ -1,11 +1,12 @@
-// import Plugin from 'src/plugin-system/plugin.class';
 import AddToCart from 'src/plugin/add-to-cart/add-to-cart.plugin';
+import DomAccess from 'src/helper/dom-access.helper';
+import HTTPClient from 'src/service/http-client.service';
 
-// export default class RumbleButton extends Plugin {
 export default class RumbleButton extends AddToCart {
     init() {
-        // For finding the functions bound to this element
-        // console.info(window.PluginManager.getPluginInstancesFromElement(this.el));
+        this._client = new HTTPClient(window.accessKey, window.contextToken);
+        this.cartEl = DomAccess.querySelector(document, '.header-cart');
+        this.PluginManager = window.PluginManager;
 
         // keep the original AddToCart init functionality
         super.init();
@@ -15,5 +16,37 @@ export default class RumbleButton extends AddToCart {
             const productName = productNameInput ? productNameInput.value.trim() : 'Unknown Product';
             console.info(`Rumble Button for "${productName}" clicked! Let's add some rumble to the shopping experience!`);
         });
+    }
+
+    _openOffCanvasCart(instance, requestUrl, formData) {
+        // // call the original AddToCart functionality
+        // super._openOffCanvasCarts(instance, requestUrl, formData);
+
+        this._client.post(requestUrl, formData, this._afterAddToCart.bind(this));
+    }
+
+    _afterAddToCart() {
+        this._refreshCartValue();
+
+        // add rumble effect to the cart icon
+        this._rumbleButton();        
+    }
+
+    _refreshCartValue() {
+        const cardWidgetEl = DomAccess.querySelector(this.cartEl, '[data-cart-widget]');
+        const cartWidgetInstance = this.PluginManager.getPluginInstanceFromElement(cardWidgetEl, 'CartWidget');
+
+        // if (cartWidgetInstance) {
+        //     cartWidgetInstance.refresh();
+        // }
+        cartWidgetInstance.fetch();
+    }
+
+    _rumbleButton() {
+        this.cartEl.classList.add('rumble');
+
+        window.setTimeout(() => {
+            this.cartEl.classList.remove('rumble');
+        }, 1000);
     }
 }
