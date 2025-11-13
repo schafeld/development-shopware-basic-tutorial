6b6bbbf7fd63645eb993f5c97a8a7405d083da5a
Oliver Schafeld <oliver@schafeld.com>
2025-11-13 11:30:45 +0100

    extend ATC buttons to log name of clicked product

--- custom/plugins/SwagTemplateAdvanced/src/Resources/app/storefront/src/main.js
+++ custom/plugins/SwagTemplateAdvanced/src/Resources/app/storefront/src/main.js
@@ -1,7 +1,11 @@
 import RumbleButton from './script/rumble-button';
 
-window.PluginManager.register('RumbleButton', RumbleButton, '.btn-buy');
+// The button only holds the RumbleButton function, we need an element higher up in the DOM that holds more plugins
+// window.PluginManager.register('RumbleButton', RumbleButton, '.btn-buy');
+// '.buy-widget' has the data attribute for AddToCart plugin
 
+// override AddToCart with RumbleButton
+window.PluginManager.override('AddToCart', RumbleButton, '[data-add-to-cart]');
 
-console.info('SwagTemplateAdvanced plugin loaded');
+console.info('SwagTemplateAdvanced plugin loaded...');
 
--- custom/plugins/SwagTemplateAdvanced/src/Resources/app/storefront/src/script/rumble-button.js
+++ custom/plugins/SwagTemplateAdvanced/src/Resources/app/storefront/src/script/rumble-button.js
@@ -1,7 +1,19 @@
-import Plugin from 'src/plugin-system/plugin.class';
+// import Plugin from 'src/plugin-system/plugin.class';
+import AddToCart from 'src/plugin/add-to-cart/add-to-cart.plugin';
 
-export default class RumbleButton extends Plugin {
+// export default class RumbleButton extends Plugin {
+export default class RumbleButton extends AddToCart {
     init() {
-        console.info(window.PluginManager.getPluginInstancesFromElement(this.el));
+        // For finding the functions bound to this element
+        // console.info(window.PluginManager.getPluginInstancesFromElement(this.el));
+
+        // keep the original AddToCart init functionality
+        super.init();
+
+        this.el.addEventListener('click', () => {
+            const productNameInput = this.el.parentElement.querySelector('input[name="product-name"]');
+            const productName = productNameInput ? productNameInput.value.trim() : 'Unknown Product';
+            console.info(`Rumble Button for "${productName}" clicked! Let's add some rumble to the shopping experience!`);
+        });
     }
 }
